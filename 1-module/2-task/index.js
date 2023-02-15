1095fb6f6d583ab01110902d71a726c6d377fe80
Jorj1990 <jenia_shabalin@mail.ru>
2023-02-15 06:19:00 +0300

    исправил решение функции валидации имени пользователя

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,7 +11,7 @@ function print(text) {
  */
 
 function isValid(name) {
-  if (!name || name.includes(" ") || !name.length >= 4) {
+  if (!name || name.includes(" ") || name.length < 4) {
     return false;
   } else {
     return true;
