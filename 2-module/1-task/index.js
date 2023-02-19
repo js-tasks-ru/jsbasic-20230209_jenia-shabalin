1503a9790f4aac78f30cd3c32a3d199aae2e051e
Jorj1990 <jenia_shabalin@mail.ru>
2023-02-19 14:04:15 +0300

    получил сумму всех значение кроме специальных NaN, infinity, -infinity

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key]) && typeof salaries[key] === typeof 1) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
