e15bd229a6e86bf73e2837be8d460e1c19a48a72
Jorj1990 <jenia_shabalin@mail.ru>
2023-03-07 20:46:29 +0300

    выбрал с помощью queryselector необходимые селектор и id. с помощью обработчика событий установил условие при клике

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+  btn.addEventListener("click", () => {
+    text.hidden == true ? (text.hidden = false) : (text.hidden = true);
+  });
 }
